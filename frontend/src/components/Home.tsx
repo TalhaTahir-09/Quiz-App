ca603affb30497a2758ab303d19cc018e29cf4ec
TalhaTahir-09 <hamzatalhabwp4@gmail.com>
2025-05-08 00:13:42 +0500

    Now using the home page to display the score aswell

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { useEffect } from "react";
 import { useNavigate } from "react-router";
-import Button from './Button.tsx'
-import 'antd/dist/reset.css';
+import Button from "./Button.tsx";
+import "antd/dist/reset.css";
 
 function Home() {
+  const search = window.location.search;
+  const searchParams = new URLSearchParams(search);
+  const score = searchParams.get("score");
   const navigate = useNavigate();
   const token = localStorage.getItem("accessToken");
 
@@ -30,27 +33,48 @@ function Home() {
     responseFunction();
   }, []);
   const handleSubmitQuiz = () => {
-    navigate("/quiz-config")
-  }
+    navigate("/quiz-config");
+  };
   const handleSubmitLead = () => {
-    navigate("/leaderboard")
-  }
+    navigate("/leaderboard");
+  };
+  if (score) {
+    return (
+      <div className="home-page-wrapper flex justify-center items-center h-svh">
+        <div className="nav-container w-2/4 bg-white px-12 pt-6 rounded-2xl">
+          <div className="header-home grid place-items-center">
+            <h1 className="text-gray-900 text-5xl font-semibold">
+              {`Your Score is ${score}`}
+            </h1>
+          </div>
 
-  return (
-    <div className="home-page-wrapper flex justify-center items-center h-svh">
-      <div className="nav-container w-2/4 bg-white px-12 pt-6 rounded-2xl">
-        <div className="header-home grid place-items-center">
-        <h1 className="text-gray-900 text-5xl font-semibold">Welcome to home!</h1>
+          <div className="button-container-home flex items-center justify-center gap-6">
+            <Button title="Start Quiz" onSubmit={handleSubmitQuiz} />
+            <Button title="LeaderBoard" onSubmit={handleSubmitLead} />
+          </div>
+          <button></button>
         </div>
-
-        <div className="button-container-home flex items-center justify-center gap-6">
-          <Button title="Start Quiz" onSubmit={handleSubmitQuiz}/>
-          <Button title="LeaderBoard" onSubmit={handleSubmitLead}/>
-        </div>
-        <button></button>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="home-page-wrapper flex justify-center items-center h-svh">
+        <div className="nav-container w-2/4 bg-white px-12 pt-6 rounded-2xl">
+          <div className="header-home grid place-items-center">
+            <h1 className="text-gray-900 text-5xl font-semibold">
+              Welcome to home!
+            </h1>
+          </div>
+
+          <div className="button-container-home flex items-center justify-center gap-6">
+            <Button title="Start Quiz" onSubmit={handleSubmitQuiz} />
+            <Button title="LeaderBoard" onSubmit={handleSubmitLead} />
+          </div>
+          <button></button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Home;
